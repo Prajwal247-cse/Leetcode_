c4da8fcc9513f162b77f279c1027a8714cd187ad
Prajwal B <prajwalb.10a2@gmail.com>
2026-07-21 23:21:42 +0530

    Time: 44 ms (98.64%), Space: 116.2 MB (8.71%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+        if (intervals.length == 0) return 0;
+
+        // Sort by end time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int count = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            // Overlap
+            if (intervals[i][0] < prevEnd) {
+                count++;
+            }
+            // No overlap
+            else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+}
